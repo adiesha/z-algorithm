aa913156b9fd396fd12ef29d1782a58e847a1b83
Adiesha <adiesha@gmail.com>
2021-09-05 13:17:52 -0600

    Read fasta file capabillity added

---
+++ readFasta.py
@@ -0,0 +1,27 @@
+def main():
+    fasta = Fasta("test.fasta")
+    print(fasta.data)
+
+
+class Fasta:
+
+    def __init__(self, filename):
+        self.fileName = filename
+        self.data = {}
+        self.file = None
+        self.lines = None
+        self.read()
+
+    def read(self):
+        self.file = open(self.fileName, 'r')
+        self.lines = self.file.readlines()
+        index = None
+        for line in self.lines:
+            if line.startswith(">"):
+                index = int(line.split("seq")[-1])
+            else:
+                self.data[index] = line
+
+
+if __name__ == '__main__':
+    main()
